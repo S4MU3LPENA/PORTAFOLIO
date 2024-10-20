b2c6084b111ae13893d05d59a6ab0183e03954be
SamuDev <102234459+S4MU3LPENA@users.noreply.github.com>
2024-10-20 07:24:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+    strings: [
+        '<i class="sisoy">Samuel Peña.<i>', 
+        '<i class="sisoy">Developer.<i>'],
+    typeSpeed: 70,
+    loop: true,
+    fadeout: true,
+    backspeed: 0,
+    backDelay: 1700
+  });
